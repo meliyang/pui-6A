5de793d68c7ae52cc43f2124f7efc5d1a6e45aa8
Melissa Yang <mwyang@andrew.cmu.edu>
2022-03-22 23:57:34 -0400

    updated with js

--- main.js
+++ main.js
@@ -1,70 +1,40 @@
 
-function RedPanda(name, age) {
-  this.name = name;
-  this.age = age;
-  this.image_alt = "red panda";
-  this.image = "img/redpanda.png";
-}
+// // Counter for items
+// var counterDisplayElem = document.getElementById("counter-display"), count = 1;
+// var counterMinusElem = document.getElementById("counter-minus");
+// var counterPlusElem = document.getElementById("counter-plus");
 
-function Bunny(name, age) {
-  this.name = name;
-  this.age = age;
-  this.image_alt = "bunny rabbit";
-  this.image = "img/bunny.png";
-}
 
-function Axolotl(name, age) {
-  this.name = name;
-  this.age = age;
-  this.image_alt = "axolotl";
-  this.image = "img/axolotl.png";
-}
 
-smiley = new RedPanda("Smiley", 10);
-orie = new Bunny("Orie", 5);
-axe = new Axolotl("Axe", 2);
+// counterPlusElem.onclick = function() {
+//     count++;
+//     counterDisplayElem.innerHTML = count;
+// } ;
 
-animal = [smiley, orie, axe];
-names = ["Smiley", "Orie", "Axe"]
+// counterMinusElem.onclick = function() {
+//     count--;
+//     counterDisplayElem.innerHTML = count;
+// };
 
-function generateRandomIndex(maxIndex) {
-  return Math.floor(Math.random() * maxIndex);
-}
 
-function generateRandomName() {
-  randomIndex = generateRandomIndex(3);
-  return names[randomIndex];
+
+function Cookie(name, count, frequency) {
+  this.name = "cookie";
+  this.count = count;
+  this.frequency = frequency;
+  this.image = "img/cookie.png";
+  this.image_alt = "chocolate chip cookie";
+
 }
 
 
-function generateRandomAge() {
-  return generateRandomIndex(100);
+
+function remove() {
+  var element = document.getElementById("cookie-1"); // TODO: remove current item, currently hard coded to delet cookie-1
+  element.remove();
 }
 
-function generateRandomAnimal() {
-  randomIdx = generateRandomIndex(3);
-  randAnimal = animal[randomIdx];
-  if (randAnimal instanceof RedPanda) {
-    myAnimal = new RedPanda(generateRandomName(), generateRandomAge());
-  }
-  else if (randAnimal instanceof Bunny) {
-    myAnimal = new Bunny(generateRandomName(), generateRandomAge());
-  }
-  else if (randAnimal instanceof Axolotl) {
-    myAnimal = new Axolotl(generateRandomName(), generateRandomAge());
-  }
-  return myAnimal;
-}
+var q = document.getElementById('quantity');
+var frq = document.getElementById('frequency');
 
-
-function onLoad() {
-    var animal = generateRandomAnimal();
-    // fill in code
-    var img = document.getElementById("randomAnimal");
-    img.setAttribute("src", animal.image);
-    img.setAttribute("alt", animal.image_alt);
-
-    document.getElementById("name").innerHTML = animal.name;
-    document.getElementById("age").innerHTML = animal.age;
-}
-
+console.log(q);
